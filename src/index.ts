fcfbf8703a0d81499bab96948e8b955e01a9cf4a
serhiisezonchyk <sergey040901@gmail.com>
2024-02-27 11:27:48 +0200

    add winstom logger/prettier imports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
-import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
-
+import express, { Express, Request, Response } from 'express';
+import { loggingHandler } from './middlewares/loggingHandler';
+import { routeNotFound } from './middlewares/routeNotFound';
+import './utils/logger';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
-const arr: number[] = [1, 2, 3, 4, 5];
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Express ${arr.at(1)}`);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Log all routes
+app.use(loggingHandler);
+
+/*
+  Your routes here
+*/
+app.get('/hi', (req: Request, res: Response) => {
+  res.status(200).json(`Express`);
 });
 
+//Log not found routes
+app.use(routeNotFound);
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  logger.info(`[server]: Server is running at http://localhost:${port}`);
 });
